d32351db985ac3012d650f71b7e3a88cbfbffcbf
jaloliddin.mahkamov@mail.ru <jaloliddin.mahkamov@mail.ru>
2025-04-23 02:12:28 +0500

    DATA: Employee Model & Migrate

---
+++ EmpLoad/Brokers/Storages/IStorageBroker.Emloyee.cs
@@ -0,0 +1,16 @@
+﻿using EmpLoad.Models.Foundations.Employees;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmpLoad.Brokers.Storages
+{
+    public partial interface IStorageBroker
+    {
+        ValueTask<Employee> InsertEmployeeAsync(Employee employee);
+        ValueTask<IQueryable<Employee>> SelectAllEmployeesAsync();
+        ValueTask<Employee> SelectEmployeeByIdAsync(int employeeId);
+        ValueTask<Employee> UpdateEmployeeAsync(Employee employee);
+        ValueTask<Employee> DeleteEmployeeAsync(Employee employee);
+
+    }
+}
